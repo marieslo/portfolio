e044ad73a566919b7bb3e929fa932f3cc6dde711
marieslo <maria.slovohotova@gmail.com>
2025-02-18 22:54:49 +0400

    skill icons has changed to bages

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,56 +8,33 @@ export default function Footer({ isDarkMode }: FooterProps) {
   return (
     <div
       className={`w-full flex items-end justify-between z-50 bg-transparent ${
-        isDarkMode ?  "text-light-text" : "text-dark-text"
+        isDarkMode ? "text-light-text" : "text-dark-text"
       } font-bodytext text-sm tracking-tight px-6 py-1`}
     >
-      <div className="flex flex-col">
+      <div className="flex flex-col space-y-1">
         <span>
-        <div className="list-disc list-inside">
-          Icons provided by: &nbsp;&nbsp;
-            <span>
-              <a
-                href="https://icons8.com"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="underline"
-              >
-                Icons8,&nbsp;
-              </a>
-            </span>
-          </div>
+          Icons provided by&nbsp;
+          <a
+            href="https://icons8.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline"
+          >
+            Icons8
+          </a>
         </span>
 
         <span>
-          <div className="list-disc list-inside">
-            <span>
-              <a
-                href="https://skillicons.dev"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="underline"
-              >
-                Skillicons
-              </a>{" "}
-              (MIT License),&nbsp;
-            </span>
-          </div>
-        </span>
-
-        <span>
-          <div className="list-disc list-inside">
-            <span>
-              <a
-                href="https://fonts.google.com/icons"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="underline"
-              >
-                Google
-              </a>{" "}
-              (Apache License 2.0)&nbsp;
-            </span>
-          </div>
+          Badges powered by&nbsp;
+          <a
+            href="https://shields.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline"
+          >
+            Shields.io
+          </a>{" "}
+          (CC0 License)
         </span>
       </div>
       <div className="flex flex-col sm:flex-row justify-end items-end">
